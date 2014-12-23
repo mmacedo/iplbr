bdc15cc873afb709bf1f57ee25de27bb6678288d
Michel Pavan Macedo <michelpm@gmail.com>
2014-12-23 15:56:23 -0200

    Corrigir e melhorar script de publicação

--- publish_to_github.sh
+++ publish_to_github.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
+function discard_local_changes() {
+  git reset HEAD -- .
+  git checkout -- .
+  git clean -df
+}
+
+function add_changes() {
+  git rm -r .
+  cp -r ../public/* .
+}
+
+function commit_changes() {
+  git add -A
+  git commit -m "$(date -u)"
+}
+
 pushd $(dirname "$0")/_deploy
+discard_local_changes
 git pull origin gh-pages
-git rm -r .
-cp -r ../public/* .
-git add -A
-git commit -m "$(date -u)"
+add_changes
+commit_changes
 git push origin gh-pages
 popd
