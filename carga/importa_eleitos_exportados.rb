3ecb1a01a8270bda6f398668cb91040af860e623
Michel Pavan Macedo <michelpm@gmail.com>
2014-12-25 20:44:16 -0200

    Completar dados de 2000

--- carga/importa_eleitos_exportados.rb
+++ carga/importa_eleitos_exportados.rb
@@ -2,13 +2,16 @@
 
 # http://www.tse.jus.br/eleicoes/eleicoes-anteriores/eleicoes-1996/resultados-das-eleicoes
 
+ano = 2000
+
 require 'set'
 require 'fileutils'
+require 'unicode'
 
 cwd = File.expand_path(File.dirname(__FILE__))
 
 # Onde está a lista de eleitos
-arquivo_de_entrada = File.expand_path("~/Downloads/eleitos1996.csv")
+arquivo_de_entrada = File.expand_path("~/Downloads/eleitos#{ano}.csv")
 
 # Onde ficarão os novos arquivos com lista de eleitos
 pasta_de_saida    = File.join(cwd, "eleitos")
@@ -28,17 +31,20 @@ tabela_de_partidos = {
   "PT"      => 13,
   "PTB"     => 14,
   "PMDB"    => 15,
+  "PSTU"    => 16, # 2000
   "PSL"     => 17,
   "PST"     => 18,
   "PTN"     => 19,
   "PSC"     => 20,
+  "PCB"     => 21, # 2000
   "PL"      => 22,
   "PPS"     => 23,
   "PFL"     => 25,
   "PAN"     => 26,
   "PRTB"    => 28,
   "PGT"     => 30,
-  "PSN"     => 31,
+  # "PSN"     => 31, # 1996
+  "PHS"     => 31, # 2000
   "PMN"     => 33,
   "PRN"     => 36,
   "PSB"     => 40,
@@ -47,6 +53,7 @@ tabela_de_partidos = {
   "PSD"     => 41,
   "PSDB"    => 45,
   "PSDC"    => 27,
+  "PRONA"   => 56, # 2000
   "PC do B" => 65,
   "PT do B" => 70
 }
@@ -54,11 +61,13 @@ tabela_de_partidos = {
 linhas = IO.readlines(arquivo_de_entrada).map do |linha|
   cargo, uf, municipio, sigla, nome = linha.chomp.split(',')
 
+  sigla = 'PPB' if sigla == 'PP' and ano == 2000
+  nome = Unicode::upcase(nome)
   numero = tabela_de_partidos[sigla]
 
   puts sigla unless tabela_de_partidos.has_key? sigla
 
-  [ "1996", uf, municipio, cargo.upcase, numero.to_s, sigla, nome ]
+  [ ano.to_s, uf, municipio, cargo.upcase, numero.to_s, sigla, nome ]
 end
 
 linhas.each do |linha|
@@ -68,6 +77,57 @@ end
 IO.write(File.join(cwd, "partidos.txt"), partidos.map { |l| l.join(";") }.join("\n"))
 
 ufs.each do |uf|
-  visao = linhas.select { |linha| linha[1] == uf }.sort
-  IO.write(File.join(pasta_de_saida, "1996_#{uf.downcase}.txt"), visao.map { |l| l.join(";") }.join("\n"))
+
+  arquivo_de_saida = File.join(pasta_de_saida, "#{ano}_#{uf.downcase}.txt")
+
+  original = IO.readlines(arquivo_de_saida).map { |linha| linha.chomp.split(';') }
+  novo     = linhas.select { |linha| linha[1] == uf }.sort
+
+  original_municipios = original.group_by { |linha| linha[2] }
+  novo_municipios     = novo.group_by { |linha| linha[2] }
+
+  novo_municipios.keys.each do |municipio|
+
+    if original_municipios.has_key? municipio
+
+      vereadores_original = original_municipios[municipio].select { |m| m[3] == 'VEREADOR' }
+      vereadores_novo     = novo_municipios[municipio].select { |m| m[3] == 'VEREADOR' }
+
+      prefeitos_original = original_municipios[municipio].select { |m| m[3] == 'PREFEITO' }
+      prefeitos_novo     = novo_municipios[municipio].select { |m| m[3] == 'PREFEITO' }
+
+      if vereadores_original.count != vereadores_novo.count
+        puts "Subtituindo vereadores de '#{municipio}' - #{uf} (de #{vereadores_original.count.to_s.rjust(2)} para #{vereadores_novo.count.to_s.rjust(2)})"
+
+        original_por_partido = vereadores_original.group_by { |x| x[5] }
+        novo_por_partido     = vereadores_novo.group_by { |x| x[5] }
+
+        novo_por_partido.keys.each do |sigla|
+          if (!original_por_partido.has_key?(sigla)) or novo_por_partido[sigla].count != original_por_partido[sigla].count
+            original_por_partido[sigla] = novo_por_partido[sigla]
+          end
+        end
+
+        vereadores_original = original_por_partido.values.reduce(:+)
+      end
+
+      if prefeitos_original.count != prefeitos_novo.count
+        puts "Adicionando prefeito de '#{municipio}' - #{uf} (de #{prefeitos_original.count.to_s} para #{prefeitos_novo.count.to_s})"
+        prefeitos_original = prefeitos_novo
+      end
+
+      original_municipios[municipio] = prefeitos_original + vereadores_original
+
+    else
+
+      puts "Adicionando '#{municipio}' - #{uf}"
+      original_municipios[municipio] = novo_municipios[municipio]
+
+    end
+
+  end
+
+  novo = original_municipios.values.reduce(:+) || []
+
+  IO.write(arquivo_de_saida, novo.sort.map { |l| l.join(";") }.join("\n"))
 end
